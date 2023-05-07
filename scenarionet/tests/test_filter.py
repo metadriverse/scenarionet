805841ac930d212b1547acfdaea7d2d9fee15838
QuanyiLi <quanyili0057@gmail.com>
2023-05-07 19:28:47 +0100

    sdc filter test

--- scenarionet/tests/test_filter.py
+++ scenarionet/tests/test_filter.py
@@ -1,10 +1,11 @@
 import os
 import os.path
-from scenarionet.builder.filters import ScenarioFilter
-from scenarionet import SCENARIONET_PACKAGE_PATH
-from scenarionet.builder.utils import combine_multiple_dataset, read_dataset_summary, read_scenario
-from scenarionet.verifier.utils import verify_loading_into_metadrive
+
 from metadrive.type import MetaDriveType
+
+from scenarionet import SCENARIONET_PACKAGE_PATH
+from scenarionet.builder.filters import ScenarioFilter
+from scenarionet.builder.utils import combine_multiple_dataset
 
 
 def test_filter_dataset():
@@ -14,21 +15,32 @@ def test_filter_dataset():
 
     output_path = os.path.join(SCENARIONET_PACKAGE_PATH, "tests", "combine")
 
-    num_condition = ScenarioFilter.make(ScenarioFilter.object_number,
-                                        number_threshold=6,
-                                        object_type=MetaDriveType.PEDESTRIAN,
-                                        condition="greater")
+    # test 1
     # nuscenes data has no light
     # light_condition = ScenarioFilter.make(ScenarioFilter.has_traffic_light)
     sdc_driving_condition = ScenarioFilter.make(ScenarioFilter.sdc_moving_dist,
-                                                target_dist=2,
+                                                target_dist=30,
                                                 condition="smaller")
-
+    answer = ['scene-0553', 'scene-0757', 'scene-1100']
     summary, mapping = combine_multiple_dataset(output_path,
                                                 *dataset_paths,
                                                 force_overwrite=True,
                                                 try_generate_missing_file=True,
-                                                filters=[num_condition, sdc_driving_condition])
+                                                filters=[sdc_driving_condition])
+    assert len(answer) == len(summary)
+    for a in answer:
+        in_ = False
+        for s in summary:
+            if a in s:
+                in_ = True
+                break
+        assert in_
+
+    # test 2
+    num_condition = ScenarioFilter.make(ScenarioFilter.object_number,
+                                        number_threshold=6,
+                                        object_type=MetaDriveType.PEDESTRIAN,
+                                        condition="greater")
 
 
 if __name__ == '__main__':
