e97068ca865863e553271e9ee7141e068500f395
QuanyiLi <quanyili0057@gmail.com>
2023-05-08 17:16:57 +0100

    fix bug

--- scenarionet/tests/local_test/combine_verify_generate.sh
+++ scenarionet/tests/local_test/combine_verify_generate.sh
@@ -3,4 +3,4 @@
 python ../../scripts/combine_dataset.py --to ../tmp/test_combine_dataset --from_datasets ../../../dataset/waymo ../../../dataset/pg ../../../dataset/nuscenes ../../../dataset/nuplan --overwrite
 python ../../scripts/verify_dataset.py --dataset_path ../tmp/test_combine_dataset --result_save_dir ../tmp/test_combine_dataset --random_drop --num_workers=16
 python ../../scripts/generate_from_error_file.py --file ../tmp/test_combine_dataset/error_scenarios_for_test_combine_dataset.json --overwrite --dataset_path ../tmp/verify_pass
-python ../../scripts/generate_from_error_file.py --file ../tmp/test_combine_dataset/error_scenarios_for_combine_dataset.json --overwrite --dataset_path ../tmp/verify_fail --broken
+python ../../scripts/generate_from_error_file.py --file ../tmp/test_combine_dataset/error_scenarios_for_test_combine_dataset.json --overwrite --dataset_path ../tmp/verify_fail --broken
