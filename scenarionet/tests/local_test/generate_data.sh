fba52b7f8ca73bee13bb1a027e223aaa90ff2200
QuanyiLi <quanyili0057@gmail.com>
2023-05-08 15:30:51 +0100

    batch generation

--- scenarionet/tests/local_test/generate_data.sh
+++ scenarionet/tests/local_test/generate_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nohup python _test_convert_nuplan.py 2>&1 &
-nohup python _test_convert_nuscenes.py 2>&1 &
-nohup python _test_convert_pg.py 2>&1 &
-nohup python _test_convert_waymo.py 2>&1 &
+nohup python _test_convert_nuplan.py > nuplan.log 2>&1 &
+nohup python _test_convert_nuscenes.py > nuscenes.log 2>&1 &
+nohup python _test_convert_pg.py > pg.log 2>&1 &
+nohup python _test_convert_waymo.py > waymo.log 2>&1 &
