cb9d8a470deee413c0e56f2212131c491b6ea5cf
QuanyiLi <quanyili0057@gmail.com>
2023-05-09 11:30:09 +0100

    add assert

---
+++ scenarionet/tests/local_test/_test_pg_multiprocess.py
@@ -0,0 +1,39 @@
+import os
+
+import tqdm
+from metadrive.envs.metadrive_env import MetaDriveEnv
+from metadrive.policy.idm_policy import IDMPolicy
+from metadrive.scenario.utils import get_number_of_scenarios, assert_scenario_equal
+
+from scenarionet import SCENARIONET_DATASET_PATH
+from scenarionet.common_utils import read_dataset_summary, read_scenario
+
+if __name__ == '__main__':
+
+    dataset_path = os.path.abspath(os.path.join(SCENARIONET_DATASET_PATH, "pg"))
+    num_scenario = get_number_of_scenarios(dataset_path)
+
+    # load multi process ret
+    summary, s_list, mapping = read_dataset_summary(dataset_path)
+    to_compare = dict()
+    for k, file in enumerate(s_list):
+        to_compare[k] = read_scenario(dataset_path, mapping, file)
+
+    # generate single process ret
+    env = MetaDriveEnv(
+        dict(
+            start_seed=0,
+            num_scenarios=num_scenario,
+            traffic_density=0.2,
+            agent_policy=IDMPolicy,
+            crash_vehicle_done=False,
+            map=2
+        )
+    )
+    policy = lambda x: [0, 1]  # placeholder
+    ret = env.export_scenarios(policy, [i for i in range(num_scenario)])
+
+    for i in tqdm.tqdm(range(num_scenario), desc="Assert"):
+        assert_scenario_equal(ret[i], to_compare[i])
+
+
