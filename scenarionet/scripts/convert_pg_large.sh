17db93317868c25f3db0e3b9beb9c30da782fc0d
QuanyiLi <quanyili0057@gmail.com>
2023-05-12 16:53:47 +0100

    add overwrite

--- scenarionet/scripts/convert_pg_large.sh
+++ scenarionet/scripts/convert_pg_large.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Author: GPT-4
+# Usage: ./script_name.sh 5000 10 /path/to/datasets 20 true
 
-# check if four arguments are passed
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 <num_scenarios_each_sub_dataset> <num_sub_datasets> <dataset_path> <num_workers>"
+# check if five arguments are passed
+if [ $# -ne 5 ]; then
+    echo "Usage: $0 <num_scenarios_sub_dataset> <num_sub_dataset> <dataset_path> <num_workers> <overwrite>"
     exit 1
 fi
 
-# get the number of scenarios, datasets, dataset path, and number of workers from command line arguments
-num_scenarios_each_sub_dataset=$1
-num_sub_datasets=$2
+# get the number of scenarios, datasets, dataset path, number of workers, and overwrite from command line arguments
+num_scenarios_sub_dataset=$1
+num_sub_dataset=$2
 dataset_path=$3
 num_workers=$4
+overwrite=$5
 
 # initialize start_index
 start_index=0
@@ -20,9 +22,13 @@ start_index=0
 for i in $(seq 0 $((num_sub_datasets-1)))
 do
   sub_dataset_path="${dataset_path}/pg_$i"
-  python -m scenarionet.scripts.convert_pg -n pg -d $sub_dataset_path --start_index=$start_index --num_workers=$num_workers --num_scenarios=$num_scenarios_each_sub_dataset
-
+  if [ "$overwrite" = true ]; then
+    python -m scenarionet.scripts.convert_pg -n pg -d $sub_dataset_path --start_index=$start_index --num_workers=$num_workers --num_scenarios=$num_scenarios_sub_dataset --overwrite
+  else
+    python -m scenarionet.scripts.convert_pg -n pg -d $sub_dataset_path --start_index=$start_index --num_workers=$num_workers --num_scenarios=$num_scenarios_sub_dataset
+  fi
+  start_index=$((start_index + num_scenarios_sub_dataset))
 done
 
 # combine the datasets
-python -m scenarionet.scripts.combine_dataset dataset_path $dataset_path --from_datasets $(for i in $(seq 0 $((num_sub_datasets-1))); do echo -n "${dataset_path}/pg_$i "; done)
+python -m scenarionet.scripts.combine_dataset dataset_path $dataset_path --from_datasets $(for i in $(seq 0 $((num_sub_dataset-1))); do echo -n "${dataset_path}/pg_$i "; done)
