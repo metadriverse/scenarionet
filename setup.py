e3a61e903111fb18535b85352791d3933e5fd5ce
QuanyiLi <quanyili0057@gmail.com>
2023-05-23 10:55:57 +0100

    fix to 3.8

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ train_requirement = ["ray==1.0.0",
 
 setup(
     name="scenarionet",
-    python_requires='==3.8',  # do version check with assert
+    python_requires='>=3.8, <3.9',  # do version check with assert
     version=version,
     description="Scalable Traffic Scenario Management System",
     url="https://github.com/metadriverse/ScenarioNet",
