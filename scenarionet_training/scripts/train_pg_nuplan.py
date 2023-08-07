7b1cc28bba3b25d9c9a3c5b19a64ead561892625
Quanyi <quanyili0057@gmail.com>
2023-08-07 05:17:58 -0700

    train script

--- scenarionet_training/scripts/train_pg_nuplan.py
+++ scenarionet_training/scripts/train_pg_nuplan.py
@@ -1,4 +1,5 @@
 import os.path
+from ray import tune
 from metadrive.envs.gym_wrapper import createGymWrapper
 from metadrive.envs.scenario_env import ScenarioEnv
 from scenarionet import SCENARIONET_REPO_PATH, SCENARIONET_DATASET_PATH
@@ -9,8 +10,8 @@ config = dict(
     env=createGymWrapper(ScenarioEnv),
     env_config=dict(
         # scenario
-        start_scenario_index=0,
-        num_scenarios=80000,
+        start_scenario_index=20000,
+        num_scenarios=40000, # 0-40000 nuplan, 40000-80000 pg
         data_directory=os.path.join(SCENARIONET_DATASET_PATH, "pg_nuplan_train"),
         sequential_seed=True,
         no_map=True,
@@ -19,7 +20,7 @@ config = dict(
 
         # curriculum training
         curriculum_level=100,
-        target_success_rate=0.75,
+        target_success_rate=0.7,
         # episodes_to_evaluate_curriculum=400,  # default=num_scenarios/curriculum_level
 
         # traffic & light
@@ -30,14 +31,14 @@ config = dict(
 
         # training scheme
         horizon=None,
-        driving_reward=4,
+        driving_reward=9,
         steering_range_penalty=1.0,
-        heading_penalty=2,
-        lateral_penalty=2.0,
+        heading_penalty=1,
+        lateral_penalty=1.0,
         no_negative_reward=True,
         on_lane_line_penalty=0,
-        crash_vehicle_penalty=2,
-        crash_human_penalty=2,
+        crash_vehicle_penalty=1,
+        crash_human_penalty=1,
         crash_object_penalty=0.5,
         # out_of_road_penalty=2,
         max_lateral_dist=2,
@@ -89,7 +90,7 @@ if __name__ == '__main__':
         config=config,
         num_gpus=args.num_gpus,
         # num_seeds=args.num_seeds,
-        num_seeds=5,
+        num_seeds=4,
         test_mode=args.test,
         # local_mode=True,
         # TODO remove this when we release our code
