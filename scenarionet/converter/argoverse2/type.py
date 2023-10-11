f168ff65c53513bbc9c10b4e82cd69c84d3a9caf
Alan-LanFeng <fenglan18@outook.com>
2023-10-11 17:58:32 +0200

    add support to av2

---
+++ scenarionet/converter/argoverse2/type.py
@@ -0,0 +1,82 @@
+import logging
+from metadrive.type import MetaDriveType
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+try:
+    from av2.datasets.motion_forecasting.data_schema import ObjectType
+    from av2.map.lane_segment import LaneType, LaneMarkType
+except ImportError as e:
+    logger.warning("Can not import av2-devkit: {}".format(e))
+
+
+def get_traffic_obj_type(av2_obj_type):
+    if av2_obj_type == ObjectType.VEHICLE or av2_obj_type == ObjectType.BUS:
+        return MetaDriveType.VEHICLE
+    # elif av2_obj_type == ObjectType.MOTORCYCLIST:
+    #     return MetaDriveType.MOTORCYCLIST
+    elif av2_obj_type == ObjectType.PEDESTRIAN:
+        return MetaDriveType.PEDESTRIAN
+    elif av2_obj_type == ObjectType.CYCLIST:
+        return MetaDriveType.CYCLIST
+    # elif av2_obj_type == ObjectType.BUS:
+    #     return MetaDriveType.BUS
+    # elif av2_obj_type == ObjectType.STATIC:
+    #     return MetaDriveType.STATIC
+    # elif av2_obj_type == ObjectType.CONSTRUCTION:
+    #     return MetaDriveType.CONSTRUCTION
+    # elif av2_obj_type == ObjectType.BACKGROUND:
+    #     return MetaDriveType.BACKGROUND
+    # elif av2_obj_type == ObjectType.RIDERLESS_BICYCLE:
+    #     return MetaDriveType.RIDERLESS_BICYCLE
+    # elif av2_obj_type == ObjectType.UNKNOWN:
+    #     return MetaDriveType.UNKNOWN
+    else:
+        return MetaDriveType.OTHER
+
+def get_lane_type(av2_lane_type):
+    if av2_lane_type == LaneType.VEHICLE or av2_lane_type == LaneType.BUS:
+        return MetaDriveType.LANE_SURFACE_STREET
+    elif av2_lane_type == LaneType.BIKE:
+        return MetaDriveType.LANE_BIKE_LANE
+    else:
+        raise ValueError("Unknown nuplan lane type: {}".format(av2_lane_type))
+
+    # DASH_SOLID_YELLOW: str = "DASH_SOLID_YELLOW"
+    # DASH_SOLID_WHITE: str = "DASH_SOLID_WHITE"
+    # DASHED_WHITE: str = "DASHED_WHITE"
+    # DASHED_YELLOW: str = "DASHED_YELLOW"
+    # DOUBLE_SOLID_YELLOW: str = "DOUBLE_SOLID_YELLOW"
+    # DOUBLE_SOLID_WHITE: str = "DOUBLE_SOLID_WHITE"
+    # DOUBLE_DASH_YELLOW: str = "DOUBLE_DASH_YELLOW"
+    # DOUBLE_DASH_WHITE: str = "DOUBLE_DASH_WHITE"
+    # SOLID_YELLOW: str = "SOLID_YELLOW"
+    # SOLID_WHITE: str = "SOLID_WHITE"
+    # SOLID_DASH_WHITE: str = "SOLID_DASH_WHITE"
+    # SOLID_DASH_YELLOW: str = "SOLID_DASH_YELLOW"
+    # SOLID_BLUE: str = "SOLID_BLUE"
+    # NONE: str = "NONE"
+    # UNKNOWN: str = "UNKNOWN"
+
+def get_lane_mark_type(av2_mark_type):
+    conversion_dict = {
+        LaneMarkType.DOUBLE_SOLID_YELLOW: "ROAD_LINE_SOLID_DOUBLE_YELLOW",
+        LaneMarkType.DOUBLE_SOLID_WHITE: "ROAD_LINE_SOLID_DOUBLE_WHITE",
+        LaneMarkType.SOLID_YELLOW: "ROAD_LINE_SOLID_SINGLE_YELLOW",
+        LaneMarkType.SOLID_WHITE: "ROAD_LINE_SOLID_SINGLE_WHITE",
+        LaneMarkType.DASHED_WHITE: "ROAD_LINE_BROKEN_SINGLE_WHITE",
+        LaneMarkType.DASHED_YELLOW: "ROAD_LINE_BROKEN_SINGLE_YELLOW",
+        LaneMarkType.DASH_SOLID_YELLOW: "ROAD_LINE_SOLID_DOUBLE_YELLOW",
+        LaneMarkType.DASH_SOLID_WHITE: "ROAD_LINE_SOLID_DOUBLE_WHITE",
+
+        LaneMarkType.DOUBLE_DASH_YELLOW: "ROAD_LINE_BROKEN_SINGLE_YELLOW",
+        LaneMarkType.DOUBLE_DASH_WHITE: "ROAD_LINE_BROKEN_SINGLE_WHITE",
+        LaneMarkType.SOLID_DASH_WHITE: "ROAD_LINE_BROKEN_SINGLE_WHITE",
+        LaneMarkType.SOLID_DASH_YELLOW: "ROAD_LINE_BROKEN_SINGLE_YELLOW",
+        LaneMarkType.SOLID_BLUE: "UNKNOWN_LINE",
+        LaneMarkType.NONE: "UNKNOWN_LINE",
+        LaneMarkType.UNKNOWN: "UNKNOWN_LINE"
+    }
+
+    return conversion_dict.get(av2_mark_type, "UNKNOWN_LINE")
